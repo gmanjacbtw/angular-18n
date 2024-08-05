079a7a7cf115a8689d8c6b3ed48d7d0e175febba
gmanjacbtw <gmanjarres@cbtw.tech>
2024-08-05 18:11:14 -0500

    Create angular i18n component

---
+++ src/app/angular-i18n/angular-i18n.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-i18n',
+  templateUrl: './angular-i18n.component.html',
+  styleUrls: ['./angular-i18n.component.scss']
+})
+export class AngularI18nComponent {
+  title = 'angular-i18n-demo';
+}
