4b0074e1b2100a1db894f61be2aa935d7deffcc0
gmanjacbtw <gmanjarres@cbtw.tech>
2024-08-05 18:11:54 -0500

    Isolate the toolbar to be used around the whole app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularI18nComponent } from './angular-i18n/angular-i18n.component';
+import { NgxTranslateComponent } from './ngx-translate/ngx-translate.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AngularI18nComponent
+  },
+  {
+    path: 'ngx-translate',
+    component: NgxTranslateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularI18nComponent } from './angular-i18n/angular-i18n.component';
+import { NgxTranslateComponent } from './ngx-translate/ngx-translate.component';
+import { ToolbarComponent } from './shared/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularI18nComponent,
+    NgxTranslateComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
